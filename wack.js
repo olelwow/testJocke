a2920c9e683a9342ab20e2bfc84db632ab4dc88c
Olle Ristenstrand <114669791+olelwow@users.noreply.github.com>
2024-08-27 09:46:33 +0200

    Update wack.js

--- wack.js
+++ wack.js
@@ -1,9 +1,10 @@
-let x = 5;
-  let y = 7;
+const x = 5;
+const y = 7;
 
 const double(x) => 
   {
-    return x*2
+    return x * 2
   }
 
-console.print("Dubbelt av 5 är " + double(x));
+console.log('Dubbelt av 5 är ' + double(x));
+console.log('Dubbelt av 7 är ' + double(y));
